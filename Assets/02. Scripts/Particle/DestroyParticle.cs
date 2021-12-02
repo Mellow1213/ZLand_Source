559f1dd399f96df4d0542a67e2c520a062bb3d66
Mellow1213 <dpcks7282@gmail.com>
2021-12-02 17:42:50 +0900

    파일 정리 및 플레이어 파티클 준비
    1. 파일 정리 / 넘버링
    2. 파티클 에셋 import

---
+++ Assets/02. Scripts/Particle/DestroyParticle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticle : MonoBehaviour {
+
+    [SerializeField] float WaitForSeconds = 1f;
+	public bool isBomb;
+
+
+	// Update is called once per frame
+	void Update () {
+        Destroy(gameObject, WaitForSeconds);		
+	}
+
+	void OnTriggerEnter(Collider other)
+    {
+		if (other.transform.gameObject.GetComponent<Health>() != null 
+			&& other.gameObject.tag != "Player"
+			&& isBomb)
+			other.transform.gameObject.GetComponent<Health>().health -= 30f;
+
+	}
+
+
+}
