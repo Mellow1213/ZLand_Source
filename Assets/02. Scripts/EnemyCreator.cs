e7de5ea37f4c5cd1cfa6bbc8653f6b0b12a83cc3
Mellow1213 <dpcks7282@gmail.com>
2021-12-07 22:59:16 +0900

    좀비 생성하는 프리팹 제작
    초 설정해서 해당 프리팹 위치에 좀비 생성하는 로직
    -> EnemyCreator 스크립트로 처리

---
+++ Assets/02. Scripts/EnemyCreator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCreator : MonoBehaviour
+{
+    public float spawnTime;
+    float timer;
+    public GameObject zombiePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        if (spawnTime == 0)
+            spawnTime = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("timer = " + timer);
+        timer += Time.deltaTime;
+        if(timer >= spawnTime)
+        {
+            timer = 0;
+            Instantiate(zombiePrefab, transform.position, Quaternion.identity);
+        }
+
+    }
+}
